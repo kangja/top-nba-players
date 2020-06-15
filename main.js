7f5014bf569aab0a742f4fade526747af97ff950
hi <kangja1996@gmail.com>
2020-06-14 22:08:46 -0400

    worked on CSS

--- main.js
+++ main.js
@@ -5,26 +5,18 @@ const giphyImg = document.querySelector("#image");
 // Get Giphy
 const getGiphy = async () => {
   try {
-    // e.preventDefault(); not sure when to use this
     const nameTwo = document.querySelector("input").value;
-    // console.log(nameTwo);
     const urlTwo = "https://api.giphy.com/v1/gifs/search";
     const apiKey = "6QonJK5XS8DaWTExlkhPXmM4DTyFXcLA";
     const finalUrl = `${urlTwo}?api_key=${apiKey}&q=${nameTwo}&limit=10&offset=0&rating=G&lang=en`;
 
     const responseTwo = await axios.get(finalUrl);
-    console.log(responseTwo);
 
-    const src = responseTwo.data.data[0].images.original.url;
-    giphyImg.src = src;
+    const url = responseTwo.data.data[0].images.original.url;
+    giphyImg.src = url;
     giphyImg.style.width = "300px";
     giphyImg.style.height = "200px";
-    giphyImg.style.border = "3px dashed red";
-
-    // giphyImg.src = responseTwo.data.data;
-    // console.log(responseTwo.data.data[0])
-
-    return src;
+    giphyImg.style.border = "6px ridge #f5d580";
   } catch (error) {
     console.log(`this is an ${error}`);
   }
@@ -42,42 +34,48 @@ const getPlayer = async (e) => {
 
     // Player Name
     const playerName2 = document.querySelector("#playerName2");
-    playerName2.innerHTML = `<strong>Player Name:</strong> ${name}`;
-    playerName2.style.border = "3px dashed blue";
+    playerName2.innerHTML = `<div class="data-container1"><strong id="player-strong">Player Name:</strong><span id="player-name">${name}</span></div>`;
+    playerName2.style.border = "6px ridge #767EA3";
+    playerName2.style.width = "191px";
+    playerName2.style.backgroundColor = "#767EA3";
 
     // Team Name
     const teamName = response.data.data[0].team.full_name;
     const teamName2 = document.querySelector("#teamName2");
-    teamName2.innerHTML = `<strong>Team Name:</strong> ${teamName}`;
-    teamName2.style.border = "3px dashed orange";
+    teamName2.innerHTML = `<div id="data-container2"><strong id="team-strong">Team Name:</strong> <span id="team-name">${teamName}</span></div>`;
+    teamName2.style.border = "6px ridge #388EC6";
+    teamName2.style.backgroundColor = "#388EC6";
 
     //Position
     const positionData = response.data.data[0].position;
     const position = document.querySelector("#position");
-    position.innerHTML = `<strong>Position:</strong> ${positionData}`;
-    position.style.border = "3px dashed green";
+    position.innerHTML = `<div id="data-container3"><strong id = "position-strong">Position:</strong> <span id="position-span">${positionData}</span></div>`;
+    position.style.border = "6px ridge #2C5D69";
+    position.style.backgroundColor = "#2C5D69";
 
     // Height
     const heightInFeet = response.data.data[0].height_feet;
     const heightInInches = response.data.data[0].height_inches;
 
     const height = document.querySelector("#height");
-    height.innerHTML = `<strong>Height:</strong> ${heightInFeet}ft ${heightInInches}inches`;
-    height.style.border = "3px dashed yellow";
+    height.innerHTML = `<div id="data-container4"><strong id="height-strong">Height:</strong> <span id="height-span">${heightInFeet}ft ${heightInInches}inches</span></div>`;
+    height.style.border = "6px ridge #848292";
+    height.style.backgroundColor = "#848292";
 
     // Weight
     const weightInLbs = response.data.data[0].weight_pounds;
     const weight = document.querySelector("#weight");
-    weight.innerHTML = `<strong>Weight:</strong> ${weightInLbs}lbs`;
-    weight.style.border = "3px dashed skyblue";
+    weight.innerHTML = `<div id="data-container5"><strong id="weight-strong">Weight:</strong> <span id="weight-span">${weightInLbs}lbs</span></div>`;
+    weight.style.border = "6px ridge #b39f86";
+    weight.style.backgroundColor = "#b39f86";
 
     playerListArray.push({
       url: giphyImg.src,
-      playerName: playerName2.textContent,
-      teamName: teamName2.textContent,
-      position: position.textContent,
-      height: height.textContent,
-      weight: weight.textContent,
+      playerName: playerName2.innerHTML,
+      teamName: teamName2.innerHTML,
+      position: position.innerHTML,
+      height: height.innerHTML,
+      weight: weight.innerHTML,
     });
   } catch (error) {
     console.log(`this is an ${error}`);
@@ -97,7 +95,6 @@ function clickSubmit(e) {
   e.preventDefault();
 
   const columns = document.querySelectorAll(".columns");
-  // console.log(columns);
   if (columns.length >= 10) {
     return null;
   }
@@ -120,6 +117,7 @@ function clickSubmit(e) {
 
   divPlayerName.addEventListener(
     "click",
+    // e is whatever i'm clicking in html and it shows what's
     function (e) {
       e = e || window.event;
       let target = e.target;
@@ -154,26 +152,19 @@ submit.addEventListener("click", clickSubmit);
 function replaceValues() {
   const image = document.querySelector("#image");
   image.src = playerListArray[num].url;
-  console.log(playerListArray);
 
   const playerName2 = document.querySelector("#playerName2");
   playerName2.innerHTML = playerListArray[num].playerName;
-  console.log(num);
-  console.log(playerListArray);
 
   const teamName2 = document.querySelector("#teamName2");
   teamName2.innerHTML = playerListArray[num].teamName;
-  console.log(teamName2);
 
   const position = document.querySelector("#position");
   position.innerHTML = playerListArray[num].position;
-  console.log(position);
 
   const height = document.querySelector("#height");
   height.innerHTML = playerListArray[num].height;
-  console.log(height);
 
   const weight = document.querySelector("#weight");
   weight.innerHTML = playerListArray[num].weight;
-  console.log(weight);
 }
